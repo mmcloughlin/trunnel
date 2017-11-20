509f68575f02c962fd7dd8528bb4db17349c53b1
Michael McLoughlin <mmcloughlin@gmail.com>
2017-11-20 14:28:59 -0800

    inspect.Constants()

--- inspect/inspect.go
+++ inspect/inspect.go
@@ -19,3 +19,17 @@ func Structs(f *ast.File) (map[string]*ast.Struct, error) {
 	}
 	return structs, nil
 }
+
+// Constants builds a map of constant name to value from the declarations in f.
+// Errors on duplicate constant names.
+func Constants(f *ast.File) (map[string]int64, error) {
+	v := map[string]int64{}
+	for _, c := range f.Constants {
+		n := c.Name
+		if _, found := v[n]; found {
+			return nil, errors.New("duplicate constant name")
+		}
+		v[n] = c.Value
+	}
+	return v, nil
+}
--- inspect/inspect_test.go
+++ inspect/inspect_test.go
@@ -36,3 +36,32 @@ func TestStructsDupe(t *testing.T) {
 	_, err := Structs(f)
 	assert.EqualError(t, err, "duplicate struct name")
 }
+
+func TestConstants(t *testing.T) {
+	f := &ast.File{
+		Constants: []*ast.Constant{
+			&ast.Constant{Name: "a", Value: 1},
+			&ast.Constant{Name: "b", Value: 2},
+			&ast.Constant{Name: "c", Value: 3},
+		},
+	}
+	v, err := Constants(f)
+	require.NoError(t, err)
+	assert.Equal(t, map[string]int64{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+	}, v)
+}
+
+func TestConstantsDupe(t *testing.T) {
+	f := &ast.File{
+		Constants: []*ast.Constant{
+			&ast.Constant{Name: "a"},
+			&ast.Constant{Name: "b"},
+			&ast.Constant{Name: "a"},
+		},
+	}
+	_, err := Constants(f)
+	assert.EqualError(t, err, "duplicate constant name")
+}
